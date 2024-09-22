8b2b28c45823933318c6b42546cdb23fdf7bd093
Tasya <chacaaulia170204@gmail.com>
2024-09-22 08:18:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/herpath/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.herpath
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Mengatur layout untuk activity
+        setContentView(R.layout.activity_main)
+    }
+}
